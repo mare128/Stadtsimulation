b2e92004f9c54f0b8bc34d698f63f902a793ae33
Mare <Mare@Ball-PC.Speedport_W_724V_01011603_05_032>
2017-07-04 17:59:03 +0200

    Klassen

---
+++ src/Simulation/SimulationCalculator.java
@@ -0,0 +1,28 @@
+package Simulation;
+
+public class SimulationCalculator {
+
+	String[][] map;
+	
+	public SimulationCalculator(){
+		enterMenu();
+	}
+	
+	public void enterMenu(){
+		
+	}
+	
+	public void loadGame(){
+		
+	}
+	
+	public void saveGame(){
+		
+		
+	}
+	
+	public void cancelGame(){
+		
+		
+	}
+}
